c49f1f65ea388a32cbcc94734b6534100e90b071
Peter Stratton <pstratton@holmanenterprises.com>
2020-06-04 15:27:06 -0400

    fixed a hardcoded repo_name value

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -6,7 +6,7 @@ from {{cookiecutter.repo_name}}.app import main
 def test_version():
     assert {{cookiecutter.repo_name}}.__version__ == '0.1.0'
 
-def test_myproject_main(capsys):
+def test_{{cookiecutter.repo_name}}_main(capsys):
     main()
     captured = capsys.readouterr()
     assert 'Welcome to {{cookiecutter.repo_name}} - v{{cookiecutter.version}}' in captured.out
