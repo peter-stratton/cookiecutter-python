c02a387272be3c23b18f118ecd1aa5a9cb46bff1
Peter Stratton <pstratton@holmanenterprises.com>
2020-06-03 22:50:30 -0400

    lint cleanup

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/app.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/app.py
@@ -1,7 +1,9 @@
 from {{cookiecutter.repo_name}} import __version__
 
+
 def main():
-    print('Welcome to {{cookiecutter.repo_name}} - v{{cookiecutter.version}}')
+    print('Welcome to {{cookiecutter.repo_name}} - v%s' % __version__)
+
 
 if __name__ == '__main__':
     main()
